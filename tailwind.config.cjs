0e3ff187f51b98dd495fde21145ef15437a3fbb2
Nagy Zétény <nagyzeteny6@gmail.com>
2022-08-19 15:10:47 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,16 @@
 module.exports = {
   content: ['./src/**/*', 'index.html'],
   theme: {
-    extend: {},
-    keyframes: {
-      block: {
-        '0%': { left: '100%' },
-        '100%': { left: '-3rem' },
+    extend: {
+      keyframes: {
+        block: {
+          '0%': { left: '100%' },
+          '100%': { left: '-3rem' },
+        },
       },
-    },
-    animation: {
-      block: 'block 2s infinite linear',
+      animation: {
+        block: 'block 2s infinite linear',
+      },
     },
   },
   plugins: [],
