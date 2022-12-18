d094785fdfcc4c7068925311b15f77b9a1d050d4
Nagy Zétény <nagyzeteny6@gmail.com>
2022-12-18 09:57:27 +0100

    latest packages, code simplification

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import './style/globals.css';
 
 let isStarted = false;
 
-const game = () => {
-  const block = document.getElementById('block')!;
-  const hole = document.getElementById('hole')!;
-  const character = document.getElementById('character')!;
-  const output = document.getElementById('output')!;
+const block = document.getElementById('block')!;
+const hole = document.getElementById('hole')!;
+const character = document.getElementById('character')!;
 
+const game = () => {
   document.getElementById('game-container')!.style.display = 'flex';
   document.getElementById('start')!.style.display = 'none';
   isStarted = true;
@@ -16,7 +15,7 @@ const game = () => {
   let score = 0;
 
   setInterval(() => {
-    output.innerText = score.toString();
+    document.getElementById('output')!.innerText = score.toString();
   }, 100);
 
   setInterval(() => {
@@ -28,9 +27,7 @@ const game = () => {
     const holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue('top'));
     const holeBottom = holeTop + 160;
 
-    if (!isJumping) {
-      character.style.top = `${characterTop + 4}px`;
-    }
+    if (!isJumping) character.style.top = `${characterTop + 4}px`;
 
     if (
       characterTop > 475 ||
@@ -50,9 +47,7 @@ const game = () => {
     const jumpInterval = setInterval(() => {
       const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
 
-      if (characterTop > 6) {
-        character.style.top = `${characterTop - 4}px`;
-      }
+      if (characterTop > 6) character.style.top = `${characterTop - 4}px`;
 
       if (jumpCount > 20) {
         clearInterval(jumpInterval);
@@ -67,16 +62,10 @@ const game = () => {
   document.addEventListener('keydown', jump);
 
   block?.addEventListener('animationiteration', () => {
-    const random = Math.floor(Math.random() * (340 + 1));
-    hole.style.top = `${random}px`;
+    hole.style.top = `${Math.floor(Math.random() * (340 + 1))}px`;
     score++;
   });
 };
 
-document.addEventListener('keydown', () => {
-  if (!isStarted) game();
-});
-
-document.querySelector('main')?.addEventListener('click', () => {
-  if (!isStarted) game();
-});
+document.addEventListener('keydown', () => !isStarted && game());
+document.querySelector('main')?.addEventListener('click', () => !isStarted && game());
